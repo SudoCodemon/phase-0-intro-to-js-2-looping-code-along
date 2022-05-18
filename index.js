3474778a9b5241dd2c06f5cd842c97affc37cce1
Daniel Yang <danyang0724@gmail.com>
2022-05-18 16:26:26 -0400

    commit tree aad46f1d8fae6236117ec484e5fcebbcf447c0c2

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function writeCards(array) {
+    return array.map((item) => `Thank you, ${item}, for the wonderful surprise gift!`)
+}
+
+function countDown() {
+    let n = 10;
+    while (n >= 0) {
+        console.log(n--);
+    }
+}
+
+console.log(countDown(10))
